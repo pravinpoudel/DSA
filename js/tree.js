ef97c02f2bd155eb4532df596504eeb8a5db220e
pravinpoudel <prvnpoudel4@gmail.com>
2020-07-02 16:45:20 +0545

    findMax, findMin and isPresence function is added in tree,js

--- js/tree.js
+++ js/tree.js
@@ -38,9 +38,56 @@ class BST {
       return searchTree(node);
     }
   }
+
+  findMax() {
+    let current = this.root;
+    while (current.right !== null) {
+      current = current.right;
+    }
+    return current.data;
+  }
+
+  findMin() {
+    let current = this.root;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
+  }
+
+  isPresent(data) {
+    console.log(data);
+    let current = this.root;
+    let check = (current) => {
+      if (data < current.data) {
+        current = current.left;
+        if (current === null) {
+          return false;
+        }
+        check(current);
+      } else if (data > current.data) {
+        current = current.right;
+        if (current === null) {
+          return false;
+        }
+        check(current);
+      } else if (data === current.data) {
+        return true;
+      }
+    };
+    check(current);
+  }
 }
 
 const tree = new BST();
 tree.add(12);
 tree.add(14);
+tree.add(22);
+tree.add(9);
+tree.add(12);
+tree.add(27);
 console.log(tree);
+console.log(tree.findMax());
+console.log(tree.findMin());
+let presence = tree.isPresent(22);
+console.log(presence);
