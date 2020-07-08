c096ddf70a800dad1bcc21e3dacd1060a2866349
pravinpoudel <prvnpoudel4@gmail.com>
2020-07-08 13:39:53 +0545

    binary search tree function code refactored with recursive function call

--- js/binarySearch.js
+++ js/binarySearch.js
@@ -1,23 +1,27 @@
-function binarySearch(arr, high = 1, low = 1, data) {
-  let mid = Math.floor((low + high) / 2);
-  console.log(mid);
-  if (arr[mid - 1] === data) {
-    console.log("data found");
-    return;
-  } else if (arr[mid - 1] > data) {
-    high = mid + 1;
-    mid = Math.floor((low + high) / 2);
-    binarySearch(arr, high, low, data);
-  } else if (arr[mid - 1] < data) {
-    low = mid + 1;
-    mid = Math.floor((low + high) / 2);
-    binarySearch(arr, high, low, data);
+function binarySearch(arr, data) {
+  let high = arr.length;
+  let low = 1;
+  function search(high, low) {
+    let mid = Math.floor((low + high) / 2);
+    console.log(arr[mid - 1]);
+    if (arr[mid - 1] === data) {
+      console.log("data found");
+      return mid - 1;
+    } else if (arr[mid - 1] > data) {
+      high = mid - 1;
+      return search(high, low);
+    } else if (arr[mid - 1] < data) {
+      low = mid + 1;
+      return search(high, low);
+    }
   }
+  return search(high, low);
 }
 
-binarySearch(
+let array = [3, 6, 8, 12, 14, 17, 25, 29, 31, 36, 42, 47, 53, 55, 62];
+let data = 17;
+let index = binarySearch(
   [3, 6, 8, 12, 14, 17, 25, 29, 31, 36, 42, 47, 53, 55, 62],
-  15,
-  1,
-  12
+  data
 );
+console.log("data you searched for is at " + index);
